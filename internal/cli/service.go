0fdfef25e2f91f552f57df02b1320e9b2de06211
Jordan Sanders <jordan@rwx.com>
2025-10-30 15:59:34 -0400

    Clean up git patch env vars (#208)

--- internal/cli/service.go
+++ internal/cli/service.go
@@ -138,7 +138,11 @@ func (s Service) InitiateRun(cfg InitiateRunConfig) (*api.InitiateRunResult, err
 
 		patchable := true
 
-		if _, ok := os.LookupEnv("RWX_DISABLE_SYNC_LOCAL_CHANGES"); ok {
+		if featureFlag, _ := os.LookupEnv("RWX_PREVIEW_GIT_PATCH"); featureFlag == "" {
+			patchable = false
+		}
+
+		if _, ok := os.LookupEnv("RWX_DISABLE_GIT_PATCH"); ok {
 			patchable = false
 		}
 
