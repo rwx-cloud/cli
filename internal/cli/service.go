9a84a44abd964394717a8f4495319cad73829133
Jordan Sanders <jordan@rwx.com>
2026-01-16 16:00:29 -0600

    Extract login service

--- internal/cli/service.go
+++ internal/cli/service.go
@@ -17,7 +17,6 @@ import (
 
 	"github.com/skratchdot/open-golang/open"
 
-	"github.com/rwx-cloud/cli/internal/accesstoken"
 	"github.com/rwx-cloud/cli/internal/api"
 	"github.com/rwx-cloud/cli/internal/dotenv"
 	"github.com/rwx-cloud/cli/internal/errors"
@@ -415,75 +414,6 @@ func outputLintJSON(w io.Writer, problems []api.LintProblem) error {
 		Problems: problems,
 	}
 	return json.NewEncoder(w).Encode(output)
-}
-
-func (s Service) Login(cfg LoginConfig) error {
-	err := cfg.Validate()
-	if err != nil {
-		return errors.Wrap(err, "validation failed")
-	}
-
-	authCodeResult, err := s.APIClient.ObtainAuthCode(api.ObtainAuthCodeConfig{
-		Code: api.ObtainAuthCodeCode{
-			DeviceName: cfg.DeviceName,
-		},
-	})
-	if err != nil {
-		return errors.Wrap(err, "unable to obtain an auth code")
-	}
-
-	// we print a nice message to handle the case where opening the browser fails, so we ignore this error
-	cfg.OpenUrl(authCodeResult.AuthorizationUrl) //nolint:errcheck
-
-	fmt.Fprintln(s.Stdout)
-	fmt.Fprintln(s.Stdout, "To authorize this device, you'll need to login to RWX Cloud and choose an organization.")
-	fmt.Fprintln(s.Stdout, "Your browser should automatically open. If it does not, you can visit this URL:")
-	fmt.Fprintln(s.Stdout)
-	fmt.Fprintf(s.Stdout, "\t%v\n", authCodeResult.AuthorizationUrl)
-	fmt.Fprintln(s.Stdout)
-	fmt.Fprintln(s.Stdout, "Once authorized, a personal access token will be generated and stored securely on this device.")
-	fmt.Fprintln(s.Stdout)
-
-	stopSpinner := Spin("Waiting for authorization...", s.StdoutIsTTY, s.Stdout)
-
-	stop := func() {
-		stopSpinner()
-		s.outputLatestVersionMessage()
-	}
-
-	for {
-		tokenResult, err := s.APIClient.AcquireToken(authCodeResult.TokenUrl)
-		if err != nil {
-			stop()
-			return errors.Wrap(err, "unable to acquire the token")
-		}
-
-		switch tokenResult.State {
-		case "consumed":
-			stop()
-			return errors.New("The code has already been used. Try again.")
-		case "expired":
-			stop()
-			return errors.New("The code has expired. Try again.")
-		case "authorized":
-			stop()
-			if tokenResult.Token == "" {
-				return errors.New("The code has been authorized, but there is no token. You can try again, but this is likely an issue with RWX Cloud. Please reach out at support@rwx.com.")
-			} else {
-				if err := accesstoken.Set(cfg.AccessTokenBackend, tokenResult.Token); err == nil {
-					fmt.Fprint(s.Stdout, "Authorized!\n")
-					return nil
-				} else {
-					return fmt.Errorf("An error occurred while storing the token: %w", err)
-				}
-			}
-		case "pending":
-			time.Sleep(cfg.PollInterval)
-		default:
-			stop()
-			return errors.New("The code is in an unexpected state. You can try again, but this is likely an issue with RWX Cloud. Please reach out at support@rwx.com.")
-		}
-	}
 }
 
 func (s Service) DownloadLogs(cfg DownloadLogsConfig) error {
