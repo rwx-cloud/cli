f8432172fcec96376359651db42f074241b917dc
Jordan Sanders <jordan@rwx.com>
2026-01-16 16:04:07 -0600

    Extract secrets service

---
+++ internal/cli/service_secrets.go
@@ -0,0 +1,86 @@
+package cli
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/rwx-cloud/cli/internal/api"
+	"github.com/rwx-cloud/cli/internal/dotenv"
+	"github.com/rwx-cloud/cli/internal/errors"
+)
+
+func (s Service) SetSecretsInVault(cfg SetSecretsInVaultConfig) error {
+	defer s.outputLatestVersionMessage()
+	err := cfg.Validate()
+	if err != nil {
+		return errors.Wrap(err, "validation failed")
+	}
+
+	secrets := []api.Secret{}
+	for i := range cfg.Secrets {
+		key, value, found := strings.Cut(cfg.Secrets[i], "=")
+		if !found {
+			return errors.New(fmt.Sprintf("Invalid secret '%s'. Secrets must be specified in the form 'KEY=value'.", cfg.Secrets[i]))
+		}
+		secrets = append(secrets, api.Secret{
+			Name:   key,
+			Secret: value,
+		})
+	}
+
+	if cfg.File != "" {
+		fd, err := os.Open(cfg.File)
+		if err != nil {
+			return errors.Wrapf(err, "error while opening %q", cfg.File)
+		}
+		defer fd.Close()
+
+		fileContent, err := io.ReadAll(fd)
+		if err != nil {
+			return errors.Wrapf(err, "error while reading %q", cfg.File)
+		}
+
+		dotenvMap := make(map[string]string)
+		err = dotenv.ParseBytes(fileContent, dotenvMap)
+		if err != nil {
+			return errors.Wrapf(err, "error while parsing %q", cfg.File)
+		}
+
+		for key, value := range dotenvMap {
+			secrets = append(secrets, api.Secret{
+				Name:   key,
+				Secret: value,
+			})
+		}
+	}
+
+	result, err := s.APIClient.SetSecretsInVault(api.SetSecretsInVaultConfig{
+		VaultName: cfg.Vault,
+		Secrets:   secrets,
+	})
+
+	if err != nil {
+		return errors.Wrap(err, "unable to set secrets")
+	}
+
+	if cfg.Json {
+		output := struct {
+			Vault      string   `json:"vault"`
+			SetSecrets []string `json:"set_secrets"`
+		}{
+			Vault:      cfg.Vault,
+			SetSecrets: result.SetSecrets,
+		}
+		if err := json.NewEncoder(s.Stdout).Encode(output); err != nil {
+			return errors.Wrap(err, "unable to encode JSON output")
+		}
+	} else if result != nil && len(result.SetSecrets) > 0 {
+		fmt.Fprintln(s.Stdout)
+		fmt.Fprintf(s.Stdout, "Successfully set the following secrets: %s", strings.Join(result.SetSecrets, ", "))
+	}
+
+	return nil
+}
