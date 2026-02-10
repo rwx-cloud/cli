a895eeb0e11a74809d085cb9e950a0fbff7f205a
Jason Robinaugh <jason@rwx.com>
2026-02-10 15:18:15 -0500

    Make it impossible to build the CLI without the LSP bundle (#340)

--- internal/lsp/embed.go
+++ internal/lsp/embed.go
@@ -4,3 +4,9 @@ import "embed"
 
 //go:embed all:bundle
 var bundle embed.FS
+
+// Compile-time check: fail the build if the LSP bundle has not been generated.
+// Run `make build` to clone and compile the language-server into internal/lsp/bundle/.
+//
+//go:embed bundle/out/server.js
+var _ string
